4d136dc8eb4a1c10bce6ee26a3685055b04dd98b
Shanwk <95632477+Shanwk@users.noreply.github.com>
2021-12-06 22:23:14 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,12 @@
+const PRIVATE_KEY='Your privatekey here'
+
+const TOKEN_ADDRESS=''; // change to Token address you want to target
+const AMOUNT=1
+const LEVEL=0.1
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
